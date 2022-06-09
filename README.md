bffb4bb28e3121289cae9fd9cf15b51b34117110
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2022-06-09 21:58:13 +0300

    All dz

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs-homework1
+
+Link to foto: https://ibb.co/album/y4DVQ2
---
+++ contacts.js
@@ -0,0 +1,52 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
+
+// Раскомментируй и запиши значение
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
